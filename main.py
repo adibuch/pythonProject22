2de7c76a2542267cdd6ced49a081ed327494ee6e
adibuch <92858769+adibuch@users.noreply.github.com>
2021-10-25 12:09:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+x=input("entet temp c :")
+x=int(x)
+x=x*9/5+32
+print(x)
+f=int(1)
+n=int(0)
+i=0
+for i in range(60):
+    if n!=f:
+        print(n)
+    n+=1
+    f*=n
+i=0
+while i<10:
+    print(i,i**2,i**3)
+    i+=1
+x=input("Enter number:")
+y=input("Enter number:")
+x=int(x)
+y=int(y)
+b=1
+while b!=0:
+    b=x%y
+    x=y
+    if b!=0:
+        y = b
+print(y)
+def fib(n):
+    i=0
+    if n<2
+        return 1
+    a=1
+    b=1
+    while i<n:
+       c=a+b
+       a=b
+       b=c
+    return c
+def fibrecursya(n):
+    if n<=2
+        return 1
+    return fibrecursya(n-1)+fibrecursya(n-2)
+def perfect_number(num):
+    sum=0
+    i=1
+    for i in range(num)
+        if num%i==0
+            sum+=i
+    if num==sum
+        print("the number ",num," is perfect")
+i-0
+for i in range(10000)
+    perfect_number(i)
+def outNumber(n1,n2):
+    num=0
+    i=0
+    x=1
+
+    for i in range(5):
+        if n1%10!=n2:
+            num+=n1%10*x
+            x*=10
+    print(num)
+num5=int(input(enter number :))
+num=int(input(enter number1:))
+
+
+
